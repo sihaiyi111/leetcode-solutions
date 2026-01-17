09e7239086a526d43859355d0be15c417a1af452
sihaiyi111 <2025681024@mails.szu.edu.cn>
2026-01-17 16:39:58 +0800

    feat: solve Climbing Stairs using iterative Fibonacci method

---
+++ math/climbStairs.c
@@ -0,0 +1,12 @@
+// math/climbStairs.c
+// feat: solve Climbing Stairs using iterative Fibonacci method
+
+int climbStairs(int n) {
+    int p = 0, q = 0, r = 1;
+    for (int i = 0; i < n; i++) {
+        p = q;
+        q = r;
+        r = p + q;
+    }
+    return r;
+}
